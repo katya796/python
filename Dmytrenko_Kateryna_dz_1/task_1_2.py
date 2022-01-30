766e4cde413140850d1438dc871abf387f45c5ee
dmytrenko.k <katyahrin796@gmail.com>
2022-01-30 10:37:54 +0200

    task_1_2

--- Dmytrenko_Kateryna_dz_1/task_1_2.py
+++ Dmytrenko_Kateryna_dz_1/task_1_2.py
@@ -1,14 +1,14 @@
 def sum_list_1(dataset: list) -> int:
     index = 0
-    finish_list = []
-    while index <= len(dataset):
+    finish_list = 0
+    while index < len(dataset):
         start = 0
         num = dataset[index]
         while num != 0:
             start = start + num % 10
             num = num // 10
-        if start % 9 == 0:
-            finish_list.append(dataset[index])
+        if start % 7 == 0:
+            finish_list = finish_list + dataset[index]
         index = index + 1
     return finish_list
 
@@ -22,7 +22,5 @@ def sum_list_2(dataset: list) -> int:
 my_list = [i ** 3 for i in range(1,1001,2)]
 result_1 = sum_list_1(my_list)
 print(result_1)
-"""result_1 = sum_list_1(my_list)
-print(result_1)
 result_2 = sum_list_2(my_list)
-print(result_2)"""
+print(result_2)
