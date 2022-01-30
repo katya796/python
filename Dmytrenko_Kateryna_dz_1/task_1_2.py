ffc1bd95fe2ebd4573acbada6c0b6f1a09644e34
dmytrenko.k <katyahrin796@gmail.com>
2022-01-30 10:43:46 +0200

    task_1_2

--- Dmytrenko_Kateryna_dz_1/task_1_2.py
+++ Dmytrenko_Kateryna_dz_1/task_1_2.py
@@ -13,10 +13,19 @@ def sum_list_1(dataset: list) -> int:
     return finish_list
 
 def sum_list_2(dataset: list) -> int:
-    #К каждому элементу списка добавляет 17 и вычисляет сумму чисел списка,
-      #  сумма цифр которых делится нацело на 7
-    # место для написания кода
-    return 1  # Верните значение полученной суммы
+    dataset_new = [i+17 for i in dataset]
+    index = 0
+    finish_list = 0
+    while index < len(dataset_new):
+        start = 0
+        num = dataset_new[index]
+        while num != 0:
+            start = start + num % 10
+            num = num // 10
+        if start % 7 == 0:
+            finish_list = finish_list + dataset_new[index]
+        index = index + 1
+    return finish_list
 
 
 my_list = [i ** 3 for i in range(1,1001,2)]
